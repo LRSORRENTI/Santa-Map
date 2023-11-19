926a0de34e2ee175a8e9870a1a42fe6a844e4232
Luke <lrsorrenti@hotmail.com>
2023-11-19 17:01:20 -0600

    added two additional christmas light colors, but big problem is that on mobile devices everything is warped...

--- src/components/ChristmasLights/ChristmasLights.js
+++ src/components/ChristmasLights/ChristmasLights.js
@@ -2,8 +2,18 @@ import React from 'react';
 import styles from './ChristmasLights.module.scss';
 
 const ChristmasLights = () => {
+  const getColorClass = (index) => {
+    switch (index % 4) { // Modulus 4 for four different colors
+      case 0: return styles.red;
+      case 1: return styles.green;
+      case 2: return styles.blue;
+      case 3: return styles.yellow;
+      default: return ''; // Default case, should not occur
+    }
+  };
+
   const lights = Array.from({ length: 60 }, (_, index) => (
-    <li key={index} className={`${styles.light} ${index % 2 === 0 ? styles.even : styles.odd}`}></li> // Apply multiple styles
+    <li key={index} className={`${styles.light} ${getColorClass(index)}`}></li>
   ));
 
   return (
